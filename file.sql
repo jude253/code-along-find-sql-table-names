9ef1e0f72736ba2c2f9056c146f4f0e7318180b6
Jude Capachietti <jcapachietti@doximity.com>
2021-08-29 01:40:42 -1000

    upload final files

--- file.sql
+++ file.sql
@@ -5,14 +5,23 @@
 select *
 from (
   select col1 from test.test_a join test.test_a1 on a.col1 = a1.col1) a
-left join test.test_b b
+left join test.test_b b # comment
 on a.col1 = b.col2
 left join
-    test.test_c c
-on b.col2  = c.col3
+    test.test_c c // comment
+/* sdfiujsdf
+iuhsidfgoisdf
+ lkjsdfgoijsdf */
+on b.col2  = c.col3 //
 left join
    (select
        col4
     from
-       test.test_d) d
+    /* sdfiujsdf
+iuhsidfgoisdf
+ lkjsdfgoijsdf */ test.test_d) d
+
+/* sdfiujsdf
+iuhsidfgoisdf
+ lkjsdfgoijsdf */
 on c.col3  = d.col4
--- main.py
+++ main.py
@@ -1,6 +1,79 @@
+def process_sql_file(file_name):
+    file, string = open(file_name, "r"), ''
 
-# this is for running the main.py:
+    # for line in file, remove comments, space out '(' and ')', add line to output string:
+    for line in file:
+        line = line.rstrip()
+        line = line.split('//')[0]
+        line = line.split('--')[0]
+        line = line.split('#')[0]
+        line = line.replace('(', ' ( ')
+        line = line.replace(')', ' ) ')
+        string += ' ' + line
+    file.close()
+
+    # remove multi-line comments:
+    while string.find('/*') > -1 and string.find('*/') > -1:
+        l_multi_line = string.find('/*')
+        r_multi_line = string.find('*/')
+        string = string[:l_multi_line] + string[r_multi_line + 2:]
+
+    # remove extra whitespaces
+    string = ' '.join(string.split())
+
+    return string
+
+
+def find_table_names(string):
+    table_names = set()
+
+    # find next closest 'from ' or 'join '
+    # take tablename after it assuming it isn't '('
+
+    while string.find('from ') > -1 or string.find('join ') > -1:
+        # find next closest 'from ' index
+        next_from_index =  float('inf')
+        if string.find('from ') > -1:
+            next_from_index = string.find('from ')
+
+        # find next closest 'join ' index
+        next_join_index = float('inf')
+        if string.find('join ') > -1:
+            next_join_index = string.find('join ')
+
+        # find the next closest keyword in string
+        # i.e. the one with the smallest index
+        next_keyword_index = min(next_from_index, next_join_index)
+
+        # set remaining string to be slice
+        # starting after next 'from ' or 'join ':
+        string = string[next_keyword_index + 5:]
+
+        # find index of next whitespace in remaining string:
+        next_space_index = string.find(' ')
+
+        # save table_name as slice from start of
+        # remaining string until next whitespace
+        table_name = string[:next_space_index]
+
+        # set remaining string as slice starting
+        # after end of table_name
+        string = string[next_space_index:]
+
+        # if the table_name isn't the start of
+        # a subquery, save it to table_names
+        if table_name != '(':
+            table_names.add(table_name)
+
+    return list(sorted(table_names))
+
+
+#  this function assumes that the .sql file does not have any syntax errors:
+def find_table_names_from_sql_file(file_name):
+    string = process_sql_file(file_name)
+    table_names = find_table_names(string)
+    return table_names
+
+
 if __name__ == '__main__':
-    print('Hello World!')
-
-
+    print(find_table_names_from_sql_file('file.sql'))
